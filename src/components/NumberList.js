d848524431d4bf6eff805362c75a219bab6ab261
ihollander <ianholla@gmail.com>
2020-12-13 09:46:31 -0500

    Added solution

--- src/components/NumberList.js
+++ src/components/NumberList.js
@@ -1,18 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { randomNumber } from "../utils";
 
-function Numbers() {
+function NumberList() {
+  const [numbers, setNumbers] = useState([]);
+
   function handleAddNumber() {
     const newNumber = randomNumber();
-    console.log(newNumber);
+    const newNumberArray = [...numbers, newNumber];
+    setNumbers(newNumberArray);
   }
+
+  function handleLiClick(numberToUpdate) {
+    const newNumberArray = numbers.map((number) =>
+      number === numberToUpdate ? numberToUpdate + 100 : number
+    );
+    setNumbers(newNumberArray);
+  }
+
+  const numberList = numbers.map((num) => (
+    <li key={num} onClick={() => handleLiClick(num)}>
+      {num}
+    </li>
+  ));
 
   return (
     <div>
       <button onClick={handleAddNumber}>Add Number</button>
-      <ul>{/* list of numbers */}</ul>
+      <ul>{numberList}</ul>
     </div>
   );
 }
 
-export default Numbers;
+export default NumberList;
--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  const [isOn, setIsOn] = useState(false);
+
+  function handleClick() {
+    setIsOn((isOn) => !isOn);
+  }
+
+  const color = isOn ? "red" : "white";
+
+  return (
+    <button style={{ background: color }} onClick={handleClick}>
+      {isOn ? "ON" : "OFF"}
+    </button>
+  );
 }
 
 export default Toggle;
