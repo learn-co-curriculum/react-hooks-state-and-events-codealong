035d4f8b26f7364ac4bf129f358e45b6747efcf0
iamchacha021 <iamchacha021@gmail.com>
2023-01-17 21:56:56 +0300

    Add a function to handle the button, it should read on and off alternatively whenever clicked

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,12 @@
 import React from "react";
+import { useState } from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  const [message, setMessage]= useState(false)
+  const handleClick=()=>{
+    setMessage((isOn)=> !isOn)
+  }
+  return <button onClick={handleClick}>{ message? "ON": "OFF"}</button>;
 }
 
 export default Toggle;
